a84895e2dc324100310931e436e2b47361847634
unknown <Mendez.AndrewM@gmail.com>
2019-05-31 18:06:12 -0700

    redefining variables or a object-oriented approach

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,47 @@
 // File wrapped with document.ready so the html & css load first
 $(document).ready(function() {
 
-// Global Variables  -subject to change ¿maybe put inside an object?
-let wins = 0
-let hint;
-let word;
-let category;
-const letterBank = [];
-const lives;
-const guess;
+//VARIABLES
+const words = ["regular", 'iregular']
 
-// get Elements
-const showCategory = $('.category');
-const getHint = $('#hint');
-const showLives = $('#lives');
-const showGuesses = $('#letterGY');
+//Empty variables to store values later
+let randomWord = "";
+let letterLength = []
+let blanks = 0;
+let blanksAndCorrect = [];
+let wrongGuess = [];
 
-
-
-// User input
-const guess = (document.onkeyup = function() {
-    guess.text = event.key;
-});
+//Counter Variables
+let wins = 0;
+let losses = 0;
+let guessesRemaining = 9;
 
 
 
+function Game() {
+    //computer generates random word from words array
+    randomWord = words[Math.floor(Math.random() * words.length)];
+
+    // split the individual word into separate arrays, and store in new array 
+    lettersOfWord = randomWord.split("");
+
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    //console logging 
+    console.log(randomWord);
+    console.log(lettersOfWord);
+    console.log(blanks);
+    console.log(blanksAndCorrect);
+}
 
 
 
